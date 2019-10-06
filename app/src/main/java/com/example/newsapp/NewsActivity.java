7c2884506d3a6cef1e4d92f35a5da2737202fa42
Abora <https://github.com/abora97>
2019-10-06 16:45:26 +0200

    create news adapter

--- app/src/main/java/com/example/newsapp/NewsActivity.java
+++ app/src/main/java/com/example/newsapp/NewsActivity.java
@@ -1,14 +1,46 @@
 package com.example.newsapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class NewsActivity extends AppCompatActivity {
+
+    @BindView(R.id.rec_news)
+    RecyclerView recNews;
+    List<String> list;
+    private RecyclerView.LayoutManager newsLayoutManager;
+    private NewsAdapter newsAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
+        ButterKnife.bind(this);
+
+        list = new ArrayList<>();
+
+        list.add("ali");
+        list.add("hana");
+        list.add("sara");
+        list.add("ali");
+
+        init();
+    }
+
+    private void init() {
+
+        newsLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        newsAdapter = new NewsAdapter(list);
+        recNews.setLayoutManager(newsLayoutManager);
+        recNews.setAdapter(newsAdapter);
     }
 }
---
+++ app/src/main/java/com/example/newsapp/NewsAdapter.java
@@ -0,0 +1,55 @@
+package com.example.newsapp;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
+
+    List<String> list;
+
+
+    public NewsAdapter(List<String> list) {
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View inflate = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_news, parent, false);
+        return new ViewHolder(inflate);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.iv_news)
+        ImageView ivNews;
+        @BindView(R.id.tv_head_line)
+        TextView tvHeadLine;
+        @BindView(R.id.tv_date)
+        TextView tvDate;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+}
